16e72a68d2d404894a1f5dd5b19afe302ad370e9
Michael Pope <amorphid@captaincoder.io>
2013-12-11 16:21:47 +1100

    Create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeyLookARailsApp::Application.config.secret_key_base = '83b3445d502d51021e43198366ee9d90684fc54826c6846c48e263d0f10e5b9541e9873e75d807c200e7cdfe586c6605e2f1fb5100750bea9841c592f98d5941'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HeyLookARailsApp::Application.config.session_store :cookie_store, key: '_hey_look_a_rails_app_session'
