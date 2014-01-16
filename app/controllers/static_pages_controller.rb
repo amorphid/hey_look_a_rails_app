372f12a8d2f67dc0a91b9301108c46b5412243b6
Michael Pope <amorphid@captaincoder.io>
2014-01-16 12:59:25 -0800

    Create static page & route for environment variables page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,29 @@
 class StaticPagesController < ApplicationController
   def environment_variables
-    @env_var1 = ENV["FOO"]
-    @env_var2 = ENV["BAR"]
-    @env_var3 = ENV["BAZ"]
+    set_foo
+    set_bar
+    set_baz
+  end
+
+private
+  def set_foo
+    @foo = case ENV["FOO"]
+           when nil then "nil"
+           else ENV["FOO"]
+           end
+  end
+
+  def set_bar
+    @bar = case ENV["BAR"]
+           when nil then "nil"
+           else ENV["BAR"]
+           end
+  end
+
+  def set_baz
+    @baz = case ENV["BAZ"]
+           when nil then "nil"
+           else ENV["BAZ"]
+           end
   end
 end
